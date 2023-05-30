28818a56f4665e76cb5e85e8df76deeef586f73f
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-05-30 06:33:34 +0530

    Custom Fetch - 1

---
+++ src/api/index.js
@@ -0,0 +1,52 @@
+import { LOCALSTORAGE_TOKEN_KEY } from '../utils';
+
+const customFetch = async (url, { body, ...customConfig }) => {
+    const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+
+    const headers = {
+        'content-type' : 'application/json',
+        Accept: 'application/json'
+    }
+
+    if (token) {
+        headers.Authorization = `Bearer ${token}`;
+    }
+
+    const config = {
+        ...customConfig,
+        headers: {
+            ...headers,
+            ...customConfig.headers,
+        },
+    };
+
+    if (body) {
+        // Stringify => is used to convert a javascript value to object notation(JSON string.
+        config.body = JSON.stringify(body);
+    }
+
+    try {
+        const response = await fetch(url, config);
+        const data = await response.json();
+
+        if (response.success) {
+            return {
+                data: data.data,
+                success: true
+            };
+        }
+
+        throw new Error(data.message);
+
+    } catch (error) {
+        console.error('error');
+        return {
+            message: error.message,
+            success: false,
+        };
+    }
+};
+
+const getPosts = (page, limit) => {
+    return customFetch();
+}
