ea4c34fcd93fcce4b0b07154ad7abf0231610717
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-06-08 20:44:45 +0530

    Created and styled the home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { getPosts } from '../api';
+import {Home} from '../pages';
+
 function App() {
   useEffect(() => {
     
@@ -12,7 +14,7 @@ function App() {
   },[]);
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,58 @@
+import styles from '../styles/home.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.postsList}>
+      <div className={styles.postWrapper}>
+        <div className={styles.postHeader}>
+          <div className={styles.postAvatar}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/4140/4140037.png"
+              alt="user-pic"
+            />
+            <div>
+              <span className={styles.postAuthor}>Ashish</span>
+              <span className={styles.postTime}>a minute ago</span>
+            </div>
+          </div>
+          <div className={styles.postContent}>Post Conetnt</div>
+
+          <div className={styles.postActions}>
+            <div className={styles.postLike}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
+                alt="likes-icon"
+              />
+              <span>5</span>
+            </div>
+
+            <div className={styles.postCommentsIcon}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/1947/1947247.png"
+                alt="comments-icon"
+              />
+              <span>2</span>
+            </div>
+          </div>
+          <div className={styles.postCommentBox}>
+            <input placeholder="Start typing a comment" />
+          </div>
+
+          <div className={styles.postCommentsList}>
+            <div className={styles.postCommentsItem}>
+              <div className={styles.postCommentHeader}>
+                <span className={styles.postCommentAuthor}>Bill</span>
+                <span className={styles.postCommentTime}>a minute ago</span>
+                <span className={styles.postCommentLikes}>22</span>
+              </div>
+
+              <div className={styles.postCommentContent}>Random comment</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
