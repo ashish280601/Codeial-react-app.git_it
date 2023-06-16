96d7062ad34caec4a452584229da52466a480e1b
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-06-16 16:12:55 +0530

    added style in login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,23 @@
+import styles from '../styles/login.module.css'
+
+
 const Login = () => {
-    return <h1>Login</h1>
+    return <form className={styles.loginForm}>
+        <span className={styles.loginSignupHeader}>Log In</span>
+
+        <div className={styles.field}>
+            <input type='email' placeholder='Email' required/>
+        </div>
+
+        <div className={styles.field}>
+            <input type='password' placeholder='Password' required/>
+        </div>
+        <div className={styles.field}>
+            <button>
+                Log In 
+            </button>
+        </div>
+    </form>
 };
 
 export default Login;
