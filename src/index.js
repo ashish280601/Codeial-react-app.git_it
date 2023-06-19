3dd96e9f0f53e82ffd65d375d94d735d1cbc10ba
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-06-19 23:36:06 +0530

    added functionality and form hooks to a login page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ToastContainer from 'react-toast-notification';
 import './styles/index.css';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <ToastContainer autoDismiss autoDismissTimeout={5000} placement="top-left">
+     <App />
+    </ToastContainer> 
+  // {/* </React.StrictMode> */}
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,65 @@
-import styles from '../styles/login.module.css'
-
+import { useState } from 'react';
+import styles from '../styles/login.module.css';
+import useToasts  from 'react-toast-notification';
+import {login} from '../api'
 
 const Login = () => {
-    return <form className={styles.loginForm}>
+    const {email, setEmail} = useState('');
+    const {password, setPassword} = useState('');
+    const {loggingIn, setLoggingIn} = useState(false);
+    const { addToast } = useToasts();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setLoggingIn(true);
+
+        if (!email || !password) {
+            return addToast('Please enter both email and password',{
+                appearance: 'error',
+            });
+        };
+
+        const response = await login(email, password);
+
+        if(response.success) {
+            addToast('Successfully logged in', {
+                appearance: 'success',
+            });
+        } else {
+            addToast(response.message, {
+                appearance: 'error',
+            });
+        }
+
+        setLoggingIn(false);
+    };
+
+    return <form className={styles.loginForm} onSubmit={handleSubmit}>
         <span className={styles.loginSignupHeader}>Log In</span>
 
         <div className={styles.field}>
-            <input type='email' placeholder='Email' required/>
+            <input 
+                type='email' 
+                placeholder='Email' 
+                required 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)}
+            />
         </div>
 
         <div className={styles.field}>
-            <input type='password' placeholder='Password' required/>
+            <input 
+                type='password'
+                placeholder='Password' 
+                required
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)}
+            />
         </div>
         <div className={styles.field}>
-            <button>
-                Log In 
+            <button disabled={loggingIn}>
+                {loggingIn ? 'Logging in...' : 'Log In'} 
             </button>
         </div>
     </form>
