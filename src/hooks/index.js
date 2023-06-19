2e17c3cefcae994d7889b943d8d20701d221a702
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-06-20 00:27:47 +0530

    Initialized are authentication

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react"
 import { AuthContext } from "../providers/AuthProvider";
 import { login as userLogin } from "../api";
+import { setItemInLocalStorage, LOCALSTORAGE_TOKEN_KEY, removeItemFromLocalStorage } from "../utils";
 
 export const useAuth = () => {
     return useContext(AuthContext)
@@ -15,6 +16,7 @@ export const useProvideAuth = () => {
 
         if (response.success) {
             setUser(response.data.user);
+            setItemInLocalStorage(LOCALSTORAGE_TOKEN_KEY, response.data.token ? response.data.token : null);
             return {
                 success: true
             };
@@ -28,6 +30,7 @@ export const useProvideAuth = () => {
 
     const logout = () => {
         setUser(null);
+        removeItemFromLocalStorage(LOCALSTORAGE_TOKEN_KEY);
     };
 
     return {
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,29 @@
 export * from './constants';
+export const setItemInLocalStorage = (key, value) => {
+    if (!key || !value) {
+        return console.error('Can not store in Local Storage');
+    }
+
+    const valueToStore = typeof value ===! 'string' ? JSON.stringify(value) : value;
+
+    localStorage.setItem(key, valueToStore);
+};
+
+export const getItemFromLocalStorage = (key) => {
+    if (!key) {
+        return console.error('Can not get the value from Local Storage');
+    }
+
+    localStorage.getItemItem(key);
+};
+
+export const removeItemFromLocalStorage = (key) => {
+    if (!key) {
+        return console.error('Can not get the value from Local Storage');
+    }
+
+    localStorage.removeItemItem(key);
+};
 
 export const getFormBody = (params) => {
     let formBody =[];
